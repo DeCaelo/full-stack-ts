125f3dbc56493d7a3f49a9333457c1add4e1d58a
DeCaelo <ludovic.cleuet@gmail.com>
2022-07-13 10:29:59 +0200

    TwitterResolverContext

--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -1,6 +1,7 @@
+import { TwitterResolverContext } from '../resolvers';
 import { QueryResolvers } from '../resolvers-types.generated';
 
-const queryTwitterResolvers: QueryResolvers = {
+const queryTwitterResolvers: QueryResolvers<TwitterResolverContext> = {
   currentUser: () => {
     return {
       id: '123',
@@ -12,8 +13,23 @@ const queryTwitterResolvers: QueryResolvers = {
       updatedAt: '',
     };
   },
-  suggestions: () => {
-    return [];
+  suggestions: (_, __, { db: _db }) => {
+    return [
+      {
+        name: 'TypeScript Project',
+        handle: 'TypeScript',
+        avatarUrl: 'http://localhost:3000/static/ts-logo.png',
+        reason: 'Because you follow @MichaelLNorth',
+        id: '1',
+      },
+      {
+        name: 'jQuery',
+        handle: 'jquery',
+        avatarUrl: 'http://localhost:3000/static/jquery-logo.jpeg',
+        reason: 'Because you follow @FrontendMasters',
+        id: '2',
+      },
+    ];
   },
 };
 export default queryTwitterResolvers;
